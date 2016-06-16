f414bd1cffd6fd124acc9e5a8be54ae0d29f0daf
Joshua Jensen <joshua.s.jensen@target.com>
2016-06-16 10:17:03 -0500

    child tcins

--- soup.py
+++ soup.py
@@ -4,24 +4,16 @@ import csv
 import requests
 import json
 import sys
+import os
+import webbrowser
 reload(sys)
 sys.setdefaultencoding('utf-8')
-htmlPaths = ["html/baby-select-diapering-wall.html",
-            "html/baby-select-health.html",
-            "html/baby-approved-bath-wall.html",
-            "html/baby-select-car-seats.html",
-            "html/baby-strollers-on-floor.html",
-            "html/baby-select-activity.html",
-            "html/nursery-on-platforms.html",
-            "html/baby-toys-0-18-months-on-shelves.html",
-            "html/prenatal-care.html",
-            "html/diaper-bags.html",
-            "html/baby-approved-potty-wall.html",
-            "html/baby-select-carriers-1.html",
-            "html/baby-select-bottles-on-shelf.html"]
+# url = "https://www.pinterest.com/heatherhortone/baby-approved-car-seats/"
+# webbrowser.open(url, new=0, autoraise=True)
+htmlPaths = os.listdir("html")
 allTcins = [];
 for file in htmlPaths:
-    with open(file) as infile:
+    with open('html/'+file) as infile:
         soup = BeautifulSoup(infile, 'html.parser')
         tcin1 = "n/a"
         tcin2 = "n/a"
@@ -35,15 +27,17 @@ for file in htmlPaths:
             for href in pin.find_all("a", class_="Button Module NavigateButton borderless hasText pinNavLink navLinkOverlay"):
                 url = href.get('href')
                 test = re.search(r"(?<!\d)\d{6,8}(?!\d)", url)
+                testForPlp = re.search(r"clearCategId", url)
                 if test:
                     tcin2 = test.group()
+                if testForPlp:
+                    tcin2 = "n/a"
             if tcin1 != "n/a":
                 tcin = tcin1
             else:
                 tcin = tcin2
-            if tcin != "n/a":
-                print(tcin)
-            else:
+            if tcin == "n/a":
+                tcin = "NO TCIN FOR: " + title
                 print("NO TCIN FOR: " + title)
             allTcins.append(tcin)
             tcin = "n/a"
@@ -52,17 +46,35 @@ for file in htmlPaths:
             title = "n/a"
 print("total items: " ,len(allTcins))
 payload = {'key': '6bf34d7581ae95886036b732'}
-csvRow = ['Concept Store','X','TCIN','DP','X','DESC,BRAND','n/a','any','x','1','X','X','X','X','X','7/18/2016','PRICE','X','7/15/2016','X','Royalston','7/18-9/9/16','Royalston']
+csvRow = ['Concept Store','X','TCIN','DP','X','DESC','BRAND','n/a','any','x','1','X','X','X','X','X','7/18/2016','PRICE','X','7/15/2016','X','Royalston','7/18-9/9/16','Royalston','scene7 image path not found']
+filteredTcins = []
+for originaltcin in allTcins:
+    tcinAlreadyFound = 'no'
+    for checkDuplicate in filteredTcins:
+        if originaltcin.isdigit() and checkDuplicate.isdigit():
+            if int(originaltcin) == int(checkDuplicate):
+                tcinAlreadyFound = 'yes'
+    if tcinAlreadyFound == 'no':
+        filteredTcins.append(originaltcin)
+print("total filtered tcins: " ,len(filteredTcins))
 with open('tcins.csv', 'wb') as fp:
     a = csv.writer(fp, delimiter=',')
-    for tcin in allTcins:
+    detectedChildren = []
+    for tcin in filteredTcins:
         print("making request for...", tcin)
-        r = requests.get('https://www.tgtappdata.com/v1/products/pdp/TCIN/'+tcin, params=payload)
-        data = json.loads(r.text)
         dpci = "n/a"
         title = "n/a"
         brand = "n/a"
+        data = "n/a"
+        if tcin.isdigit():
+            r = requests.get('https://www.tgtappdata.com/v1/products/pdp/TCIN/'+tcin, params=payload)
+            data = json.loads(r.text)
+        hasAlreadyAddedTcins = 'no'
+
         if type(data) is list:
+            isColor = 'none'
+            colors = []
+            childTcins = []
             for key, value in data[0].items():
                 if key == 'dpci':
                     dpci = value.split("-")[0]
@@ -73,9 +85,50 @@ with open('tcins.csv', 'wb') as fp:
                 if key == 'variations':
                     for key2, value2 in value[0].items():
                         if key2 == 'dpci':
-                            dpci = value2 .split("-")[0]
-        csvRow[2] = tcin
-        csvRow[3] = dpci
-        csvRow[5] = title
-        csvRow[6] = brand
-        a.writerow(csvRow)
+                            dpci = value2.split("-")[0]
+                if key == 'variationDimensions':
+                    if value['dimension1']:
+                        if value['dimension1'] == 'COLOR':
+                            isColor = 'dimension1'
+                    try:
+                        if value['dimension2']:
+                            if value['dimension2'] == 'COLOR':
+                                isColor = 'dimension2'
+                    except KeyError:
+                        # Key is not present
+                        pass
+
+                if key == 'variations':
+
+                    for t in range(len(value)):
+                        colorAlreadyAdded = 'no'
+                        for key5, value5 in value[t].items():
+                            if key5 == 'variationValues':
+                                if (isColor == 'dimension1') or (isColor == 'dimension2'):
+                                    for clr in colors:
+                                        if clr == value5[isColor]:
+                                            colorAlreadyAdded = 'yes'
+                                    if colorAlreadyAdded == 'no':
+                                        colors.append(value5[isColor])
+                                        for key4, value4 in value[t].items():
+                                            if key4 == 'tcin':
+                                                currentVaraitionTcin = int(value4)
+                                                childTcins.append(currentVaraitionTcin)
+                    if (isColor == 'dimension1') or (isColor == 'dimension2'):
+                        hasAlreadyAddedTcins = 'yes'
+                        for childItem in range(len(childTcins)):
+                            newRow = csvRow
+                            newRow[2] = childTcins[childItem]
+                            newRow[3] = dpci
+                            newRow[5] = title
+                            newRow[6] = brand
+                            newRow[24] = "http://scene7.targetimg1.com/is/image/Target/"+str(childTcins[childItem])
+                            a.writerow(newRow)
+        if hasAlreadyAddedTcins == 'no':
+            csvRow[2] = tcin
+            csvRow[3] = dpci
+            csvRow[5] = title
+            csvRow[6] = brand
+            if tcin != "n/a":
+                csvRow[24] = "http://scene7.targetimg1.com/is/image/Target/"+tcin
+            a.writerow(csvRow)
